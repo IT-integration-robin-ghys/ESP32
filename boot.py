f153279cb3f41c8f1a23fee8e64fcffea95f8ea8
RobinGhysUcll <robin.ghys@student.ucll.be>
2026-06-04 00:49:30 +0200

    Add default settings and create settings.json if missing

--- boot.py
+++ boot.py
@@ -1,6 +1,21 @@
 import os
+import json
 
 files = os.listdir()
+
+default_settings = {
+    "Test": 1
+}
+
+if "settings.json" not in files:
+    try:
+        with open("settings.json", "w") as f:
+            f.write(json.dumps(default_settings))
+        print("settings.json created")
+        files.append("settings.json")
+    except Exception as e:
+        print("Something went wrong while creating the settings file: "+e)
+
 if len(files) > 1:
     print('The device have %d files' % len(files))
     for i in range(len(files)):
