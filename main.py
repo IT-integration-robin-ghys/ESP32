93360ff253ffd8ee1c5200ab5fc35f1d107d9108
RobinGhysUcll <robin.ghys@student.ucll.be>
2026-08-26 02:30:44 +0200

    add POST settings request

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from machine import Pin, PWM, I2C
 from servo import Servo
 from time import sleep_ms
 import BME280
-from functions import web_page, return_data, process_wifi, process_email, reboot, check_and_save_apikey, send_sensor_data, control_heater, control_cooling, control_humidity, get_settings
+from functions import web_page, return_data, process_wifi, process_email, reboot, check_and_save_apikey, send_sensor_data, control_heater, control_cooling, control_humidity, get_settings, process_settings
 
 try:
     import usocket as socket
@@ -106,6 +106,8 @@ while True:
             response = return_data(bme)
         elif "GET /settings" in request:
             response = get_settings()
+        elif "POST /settings" in request:
+            response = process_settings(request)
         elif "POST /wifi" in request:
             response, reboot_after_response = process_wifi(request)
         elif "POST /email" in request:
